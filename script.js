ed70334b79e08638a81bc8ca77b7454193929c3b
RavirajkPatil <ravirajkpatil06@gmail.com>
2023-03-04 08:17:15 +0530

    changed the variable names for substraction

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function substract(a,b){
-    var c = a-b;
-    console.log("Substraction result= "+c);
+function substract(num1,num2){
+    var sub = num1-num2;
+    console.log("Substraction result = "+sub);
 }
-
 substract(10,5);
