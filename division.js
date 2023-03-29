1c355dacef6b87c0423670c5a1e829e470f91ef0
Priya <tambulkarpriya133@gmail.com>
2023-03-29 07:29:56 +0530

    Deleted FIles

--- division.js
+++
@@ -1,5 +0,0 @@
-function division(num1,num2){
-    var div = num1/num2;
-    console.log("division result = "+div);
-}
-division(10,5);
--- script.js
+++
@@ -1,5 +0,0 @@
-function substract(num1,num2){
-    var sub = num1-num2;
-    console.log("Substraction result = "+sub);
-}
-substract(10,5);
